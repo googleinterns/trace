519470f80f5ad2fcdef945811b71eaf760523e2f
claudia <cjcoulibaly@gmail.com>
2020-07-21 21:58:13 +0000

    Center on GooglePLex

--- src/main/webapp/indexScript.js
+++ src/main/webapp/indexScript.js
@@ -1,6 +1,6 @@
 function createMap() {
   const map = new google.maps.Map(
       document.getElementById('map'),
-      {center: {lat: 47.5667, lng: -122.3868}, zoom: 13, mapTypeId: 'satellite'}); 
+      {center: {lat: 37.422, lng: 122.0841}, zoom: 13, mapTypeId: 'satellite'}); 
   var infoWindow = new google.maps.InfoWindow({});
 }
