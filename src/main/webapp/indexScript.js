5cf41aef9180919b8928461e89854309d8e2b6a4
claudia <cjcoulibaly@gmail.com>
2020-07-21 22:03:46 +0000

    What do we need?

--- src/main/webapp/indexScript.js
+++ src/main/webapp/indexScript.js
@@ -4,5 +4,4 @@ function createMap() {
   const map = new google.maps.Map(
       document.getElementById('map'),
       {center: {lat: 37.422, lng: 122.0841}, zoom: 13, mapTypeId: 'satellite'}); 
-  var infoWindow = new google.maps.InfoWindow({});
 }
