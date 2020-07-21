444636fcfbd30651a19e2cd1fdb1965ed2333259
claudia <cjcoulibaly@gmail.com>
2020-07-21 21:49:19 +0000

    Added map to screen

---
+++ src/main/webapp/indexScript.js
@@ -0,0 +1,7 @@
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 47.5667, lng: -122.3868}, zoom: 13, mapTypeId: 'satellite'}); 
+  var infoWindow = new google.maps.InfoWindow({});
+
+}
