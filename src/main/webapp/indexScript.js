288713daff870adda058695621df61b0869eaebd
claudia <cjcoulibaly@gmail.com>
2020-07-21 21:56:48 +0000

    map

--- src/main/webapp/indexScript.js
+++ src/main/webapp/indexScript.js
@@ -3,5 +3,4 @@ function createMap() {
       document.getElementById('map'),
       {center: {lat: 47.5667, lng: -122.3868}, zoom: 13, mapTypeId: 'satellite'}); 
   var infoWindow = new google.maps.InfoWindow({});
-
 }
