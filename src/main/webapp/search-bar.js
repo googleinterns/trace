4febab349e37d5c93f75d84da5bb43b56ca17c1c
Erick <enriquezerick182@gmail.com>
2020-07-22 23:32:38 +0000

    Style log-in button and create placeholder for log-in click.

--- src/main/webapp/search-bar.js
+++ src/main/webapp/search-bar.js
@@ -1,6 +1,6 @@
-console.log("asdasd");
 const clearIcon = document.querySelector(".clear-icon");
 const searchBar = document.querySelector(".search");
+const logInButton = document.querySelector("#logIn");
 
 searchBar.addEventListener("keyup", () => {
   if(searchBar.value && clearIcon.style.visibility != "visible"){
@@ -13,4 +13,8 @@ searchBar.addEventListener("keyup", () => {
 clearIcon.addEventListener("click", () => {
   searchBar.value = "";
   clearIcon.style.visibility = "hidden";
+});
+
+logInButton.addEventListener("click", () => {
+    return; // TO-DO: Fetch log-in info here.
 });
