495dc3498918e37f0baed6be93fe3c2c649d6120
claudia <cjcoulibaly@gmail.com>
2020-07-22 20:33:04 +0000

    Negated lng coordinate so would actually be at googleplex

--- src/main/webapp/index-script.js
+++ src/main/webapp/index-script.js
@@ -3,5 +3,5 @@
 function createMap() {
   const map = new google.maps.Map(
       document.getElementById('map'),
-      {center: {lat: 37.422, lng: 122.0841}, zoom: 13, mapTypeId: 'satellite'}); 
+      {center: {lat: 37.422, lng: -122.0841}, zoom: 13, mapTypeId: 'satellite'}); 
 }
