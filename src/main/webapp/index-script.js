224cc795c246ba40b041cd68640c389572888ea0
claudia <cjcoulibaly@gmail.com>
2020-07-23 20:13:29 +0000

    Added comment with the places code

--- src/main/webapp/index-script.js
+++ src/main/webapp/index-script.js
@@ -9,6 +9,25 @@ function createMap() {
       document.getElementById('map'),
       {center: {lat: 37.422, lng: -122.0841}, zoom: 13, mapTypeId: 'satellite'}); 
 }
+/*  // Initialize the map!
+  const googleplex = {lat: 37.422, lng: -122.0841};
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: googleplex,
+    zoom: 13
+  });
+
+  // Create the places service.
+  const service = new google.maps.places.PlacesService(map);
+  
+  // Perform a nearby search.
+  service.nearbySearch(
+    { location: googleplex, radius: 500, type: "food" },
+    (results, status, pagination) => {
+      if (status !== "OK") return;
+      createMarkers(results, map);
+    }
+  );
+*/
 
 // Activates functionality for search bar and log-in button.
 function loadMainButtons() {
