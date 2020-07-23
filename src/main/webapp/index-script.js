b78b47d3b5bd411277ee034c46dce26ef3f87343
Erick <enriquezerick182@gmail.com>
2020-07-23 20:38:56 +0000

    Change map type to roadmap.

--- src/main/webapp/index-script.js
+++ src/main/webapp/index-script.js
@@ -7,7 +7,7 @@ function loadPage() {
 function createMap() {
   const map = new google.maps.Map(
       document.getElementById('map'),
-      {center: {lat: 37.422, lng: -122.0841}, zoom: 13, mapTypeId: 'satellite'}); 
+      {center: {lat: 37.422, lng: -122.0841}, zoom: 13}); 
 }
 
 // Activates functionality for search bar and log-in button.
