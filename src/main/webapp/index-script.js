71a8c1d0a254fd8133267bfbe0849a1c523925ef
claudia <cjcoulibaly@gmail.com>
2020-07-23 20:10:35 +0000

    Trying to implement Places API

--- src/main/webapp/index-script.js
+++ src/main/webapp/index-script.js
@@ -1,7 +1,22 @@
 
-// Creates a map centered at the Googleplex!
+// Creates a map centered at the Googleplex
 function createMap() {
-  const map = new google.maps.Map(
-      document.getElementById('map'),
-      {center: {lat: 37.422, lng: -122.0841}, zoom: 13, mapTypeId: 'satellite'}); 
+  // Initialize the map!
+  const googleplex = {lat: 37.422, lng: -122.0841};
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: googleplex,
+    zoom: 13
+  });
+
+  // Create the places service.
+  const service = new google.maps.places.PlacesService(map);
+  
+  // Perform a nearby search.
+  service.nearbySearch(
+    { location: googleplex, radius: 500, type: "food" },
+    (results, status, pagination) => {
+      if (status !== "OK") return;
+      createMarkers(results, map);
+    }
+  );
 }
