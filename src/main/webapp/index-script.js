ea7846174ae73a482216c1b7396c9a7654f2c5fa
Erick <enriquezerick182@gmail.com>
2020-07-23 20:38:56 +0000

    Change map type to roadmap.

--- src/main/webapp/index-script.js
+++ src/main/webapp/index-script.js
@@ -8,9 +8,9 @@ function loadPage() {
 
 // Creates a map centered at the Googleplex!
 function createMap() {
-  map = new google.maps.Map(
+  const map = new google.maps.Map(
     document.getElementById('map'),
-    {center: {lat: 37.422, lng: -122.0841}, zoom: 13, mapTypeId: 'satellite'}); 
+    {center: {lat: 37.422, lng: -122.0841}, zoom: 13}); 
 }
 
 // Activates functionality for search bar and log-in button.
