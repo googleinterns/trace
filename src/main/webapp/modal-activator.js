62c401565a2ea082ab4a03c8ad66520e5fb2ed47
Dominic <dterrones31@gmail.com>
2020-07-24 16:02:34 +0000

    Modal click switching MVP.

--- src/main/webapp/modal-activator.js
+++ src/main/webapp/modal-activator.js
@@ -2,9 +2,27 @@ function setupModalButtons() {
   const modalTriggers = document.querySelectorAll('[data-modal-target]');
   const modalClosers = document.querySelectorAll('[data-modal-close-button]');
   
+  modalTriggers.forEach(button => {
+    button.addEventListener('click', () => {
+      const modal = document.querySelector(button.dataset.modalTarget);
+      triggerModal(modal);
+    })
+  })
+
+  modalClosers.forEach(button => {
+    button.addEventListener('click', () => {
+      const modal = button.closest('.modal');
+      closeModal(modal);
+    })
+  })
 }
 
 function triggerModal(modal) {
   overlay.classList.add('active');
   modal.classList.add('active');
+}
+
+function closeModal(modal) {
+  overlay.classList.remove('active');
+  modal.classList.remove('active');
 }
