75ec0af240a6730264c4ad76b037093172df97e1
Dominic <dterrones31@gmail.com>
2020-07-24 15:58:43 +0000

    Function outlines for modal activation and button population.

--- src/main/webapp/modal-activator.js
+++ src/main/webapp/modal-activator.js
@@ -1,3 +1,10 @@
-function trigger-iFrame() {
+function setupModalButtons() {
+  const modalTriggers = document.querySelectorAll('[data-modal-target]');
+  const modalClosers = document.querySelectorAll('[data-modal-close-button]');
   
+}
+
+function triggerModal(modal) {
+  overlay.classList.add('active');
+  modal.classList.add('active');
 }
