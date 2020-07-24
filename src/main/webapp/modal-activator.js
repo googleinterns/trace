d7665f293fd6689dd14a9240934ad2cb83421cf8
Dominic <dterrones31@gmail.com>
2020-07-24 20:21:22 +0000

    Add comments for styling.

--- src/main/webapp/modal-activator.js
+++ src/main/webapp/modal-activator.js
@@ -1,3 +1,6 @@
+/* init() function
+ * Collects the button objects on the page and populates them with relevenat action events.
+ */
 function setupModalButtons() {
   const modalTriggers = document.querySelectorAll('[data-modal-target]');
   const modalClosers = document.querySelectorAll('[data-modal-close-button]');
@@ -8,7 +11,6 @@ function setupModalButtons() {
       triggerModal(modal);
     })
   })
-
   modalClosers.forEach(button => {
     button.addEventListener('click', () => {
       const modal = button.closest('.modal');
@@ -17,12 +19,18 @@ function setupModalButtons() {
   })
 }
 
+/* open modal function
+ * This triggers the modal, and overlay, to follow the active CSS styling, making it appear.
+ */
 function triggerModal(modal) {
   if (modal == null) return;
   overlay.classList.add('active');
   modal.classList.add('active');
 }
 
+/* close modal
+ * Undoes the modal opening, by removing the active classifier.
+ */
 function closeModal(modal) {
   if (modal == null) return;
   overlay.classList.remove('active');
