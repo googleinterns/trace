76fc9b39bcbef852492afd8b3abf4a543243b241
Erick <enriquezerick182@gmail.com>
2020-08-04 05:58:50 +0000

    Fix typo.

--- src/main/java/com/google/sps/servlets/ReviewServlet.java
+++ src/main/java/com/google/sps/servlets/ReviewServlet.java
@@ -51,7 +51,7 @@ public class ReviewServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     UserService userService = UserServiceFactory.getUserService();
 
-    String userEmail = userService.getCurrentUser().getEmail(); // Used to restrict uset to one review/location.
+    String userEmail = userService.getCurrentUser().getEmail(); // Used to restrict user to one review/location.
     String newReview = request.getParameter("comment");
     String firstName = request.getParameter("firstname");
     String lastName = request.getParameter("lastname");
