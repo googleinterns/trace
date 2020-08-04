124ef786102697ffca649a5ed06246c4c205c4da
Erick <enriquezerick182@gmail.com>
2020-08-04 05:53:36 +0000

    Load stub place_id into review form.

--- src/main/webapp/newReview.js
+++
@@ -1,9 +0,0 @@
-var currentLocation = "dsfsdf";
-
-/* Load currentLocation into newReview form. */
-function loadPlaceID() {
-  console.log("loading....");
-  console.log(currentLocation);
-  let value = document.getElementByID("place_id").value;
-  value = currentLocation;
-}
