20fe16174f433d57073a79959393adbc1193b668
Dominic <dterrones31@gmail.com>
2020-08-05 20:11:41 +0000

    Functionality methods for PlaceReviews.

--- src/main/java/com/google/sps/data/PlaceReviews.java
+++ src/main/java/com/google/sps/data/PlaceReviews.java
@@ -5,7 +5,34 @@ import java.util.ArrayList;
 public class PlaceReviews {
 
   private final int placeID;
-  private final ArrayList<Comment> reviews;
-  private final int rating;
-  private final RatingHistory history;
+  private ArrayList<Comment> reviews;
+  private double rating;
+  private int entries = 0;
+  private RatingHistory history;
+
+  public PlaceReviews(int placeID) {
+    this.placeID = placeID;
+  }
+
+  public PlaceReviews(int placeID, Comment firstReview, double initialRating) {
+    this.placeID = placeID;
+    this.reviews.append(firstReview);
+    this.rating = initialRating;
+    this.entries++;
+  }
+
+  public addReview(Comment review) {
+    this.reviews.append(review);
+  }
+
+  public addRating(double rating) {
+    if (rating > 10) {
+      addRating(10);
+    } else if (rating < 0) {
+      addRating(0);
+    } else {
+      this.rating += (rating / entries);
+    }
+  }
+
 }
