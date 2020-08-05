e705db7e976059d0afd30ead0ea5db76736cebb0
Dominic <dterrones31@gmail.com>
2020-08-05 19:36:47 +0000

    Create PlaceReviews.java as object class for internal places record keeping.

---
+++ src/main/java/com/google/sps/data/PlaceReviews.java
@@ -0,0 +1,11 @@
+package com.google.sps.data;
+
+import java.util.ArrayList;
+
+public class PlaceReviews {
+
+  private final int placeID;
+  private final ArrayList<Comment> reviews;
+  private final int rating;
+  private final RatingHistory history;
+}
