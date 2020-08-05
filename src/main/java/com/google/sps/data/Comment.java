f996c649e50bebe3f30e8848b047ed03e0f1018f
Dominic <dterrones31@gmail.com>
2020-08-05 20:53:19 +0000

    Comment.java comparator addition.

--- src/main/java/com/google/sps/data/Comment.java
+++ src/main/java/com/google/sps/data/Comment.java
@@ -8,6 +8,13 @@ public class Comment {
   private final String messageContent;
   private final Date timeStamp;
 
+  public static final Comparator<Comment> ORDER_BY_DATE = new Comparator<Comment>() {
+    @Override
+    public int compare(Comment a, Comment b) {
+      return a.getTime().compareTo(b.getTime());
+    }
+  }
+  
   public Comment(String author, String message, Date timeStamp) {
     this.author = author;
     this.messageContent = message;
@@ -30,5 +37,4 @@ public class Comment {
     this.messageContent = message;
     this.timeStamp = timeStamp;
   }
-
 }
