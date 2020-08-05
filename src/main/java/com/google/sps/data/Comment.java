ee9574e7f16f4fe034954516511fc88cd9353d12
Dominic <dterrones31@gmail.com>
2020-08-05 20:44:26 +0000

    Comment.java constructor and accessor methods.

--- src/main/java/com/google/sps/data/Comment.java
+++ src/main/java/com/google/sps/data/Comment.java
@@ -8,4 +8,22 @@ public class Comment {
   private final String messageContent;
   private final Date timeStamp;
 
+  public Comment(String author, String message, Date timeStamp) {
+    this.author = author;
+    this.messageContent = message;
+    this.timeStamp = timeStamp;
+  }
+
+  public String getAuthor() {
+    return this.author;
+  }
+
+  public String getMessage() {
+    return this.messageContent;
+  }
+
+  public Date getTime() {
+    return this.timeStamp;
+  }
+
 }
