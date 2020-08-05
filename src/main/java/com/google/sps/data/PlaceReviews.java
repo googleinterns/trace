d4bb2d99b3e11940a53c27615a7c9a740155ef8b
Dominic <dterrones31@gmail.com>
2020-08-05 23:07:28 +0000

    Add function comments.

--- src/main/java/com/google/sps/data/PlaceReviews.java
+++ src/main/java/com/google/sps/data/PlaceReviews.java
@@ -5,6 +5,12 @@ import java.util.HashSet;
 import java.com.sps.data.Comment;
 import java.com.sps.data.RatingHistory;
 
+/** PlaceReviews object class
+ * The PlaceReviews object, used as the master object detailing a locations reviews and ratings
+ * Uses implementations of smaller Comment and RatingHistory class.
+ * Does not permit multiple reviews by one author for a given location.
+ * Attempts to avoid PlaceReviews objects without any ratings or reviews.
+ */
 public class PlaceReviews {
 
   private final int placeID;
@@ -13,10 +19,16 @@ public class PlaceReviews {
   private double rating;
   private RatingHistory history;
 
+  /** Constructor
+   * This is the minimal, and avoided, constructor that makes an object without reviews.
+   */
   public PlaceReviews(int placeID) {
     this.placeID = placeID;
   }
 
+  /** Constructor
+   * This constructor provides an initial rating and review.
+   */
   public PlaceReviews(int placeID, Comment firstReview, double initialRating) {
     this.placeID = placeID;
     this.reviews.append(firstReview);
@@ -24,12 +36,19 @@ public class PlaceReviews {
     this.reviewers.add(firstReview.getAuthor());
   }
 
+  /** Updater method
+   * Adds a review to the existing PlaceReviews list of reviews
+   */
   public addReview(Comment review) {
     if (!reviewers.contains(review.getAuthor())) {
       this.reviews.append(review);
     }
   }
 
+  /** Updater method
+   * This adds a rating to a given PlaceReviews aggregate rating
+   * This does not permit a rating outside of [0, 10]
+   */
   public addRating(double rating) {
     if (rating > 10) {
       addRating(10);
@@ -39,5 +58,4 @@ public class PlaceReviews {
       this.rating += (rating / entries);
     }
   }
-
 }
