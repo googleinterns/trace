e804a93f61ae7623ab1c06b98d3c13f60a7c90fa
Dominic <dterrones31@gmail.com>
2020-08-05 19:37:55 +0000

    Create Comment.java as object class for managing individual reviews per location.

---
+++ src/main/java/com/google/sps/data/Comment.java
@@ -0,0 +1,11 @@
+package com.google.sps.data;
+
+import java.util.Date;
+
+public class Comment {
+
+  private final String author;
+  private final String messageContent;
+  private final Date timeStamp;
+
+}
