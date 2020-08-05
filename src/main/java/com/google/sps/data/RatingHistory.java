11c666c0293f64f302656a0e6c1e127ef47ba0e9
Dominic <dterrones31@gmail.com>
2020-08-05 20:57:11 +0000

    Create constructor and accessor methods.

--- src/main/java/com/google/sps/data/RatingHistory.java
+++ src/main/java/com/google/sps/data/RatingHistory.java
@@ -1,10 +1,20 @@
 package com.google.sps.data;
 
 import java.util.Date;
+import java.util.List;
 
 public class RatingHistory {
 
   private final Date start;
   private int length;
+  private List<double> history;
+
+  public RatingHistory(Date start) {
+    this.start = start;
+  }
+
+  public List<double> getHistory() {
+    return this.history();
+  }
 
 }
