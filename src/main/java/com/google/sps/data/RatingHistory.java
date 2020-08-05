7085ef45203d89c69a1155f7b8653051d74ff487
Dominic <dterrones31@gmail.com>
2020-08-05 23:09:20 +0000

    Add function comments.

--- src/main/java/com/google/sps/data/RatingHistory.java
+++ src/main/java/com/google/sps/data/RatingHistory.java
@@ -3,18 +3,30 @@ package com.google.sps.data;
 import java.util.Date;
 import java.util.List;
 
+/** RatingHistory Class
+ * This object class is used to track the history of a locations aggregate score each month
+ * Used as an object in the PlaceReviews
+ */
 public class RatingHistory {
 
   private final Date start;
   private int length;
   private List<double> history;
 
+  /** Constructor
+   * Requires a start date for its constructor
+   */
   public RatingHistory(Date start) {
     this.start = start;
   }
 
+  /** Accessor Method
+   * Returns the array of aggregates
+   */
   public List<double> getHistory() {
     return this.history();
   }
+
+  // TODO: Implement an updater function
 
 }
