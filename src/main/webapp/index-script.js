e14716d1c0e89efcab28d3b7e7c5819901d20aff
Dominic <dterrones31@gmail.com>
2020-08-12 05:20:56 +0000

    Change second parameter in fetchReviews a default param.

--- src/main/webapp/index-script.js
+++ src/main/webapp/index-script.js
@@ -403,7 +403,7 @@ function displayReviewModal() {
  * Queries ReviewServlet with elementID// Comment object gets it's datastore id. 
     comment.setId(reviewEntity.getKey().getId()); to find internal datastore
  */
-function fetchReviews(placeID, sort) {
+function fetchReviews(placeID, sort='recent') {
   const request = '/review?place_id=' + placeID + '&sort=' + sort;
   fetch(request).then(response => response.json()).then((place) => {
     populateReviews(place.reviews, placeID);
