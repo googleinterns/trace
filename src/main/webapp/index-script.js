020a732a358cdfceefa8eff7255c9bb4118f512c
claudia <cjcoulibaly@gmail.com>
2020-08-12 18:25:16 +0000

    Comments

--- src/main/webapp/index-script.js
+++ src/main/webapp/index-script.js
@@ -480,6 +480,7 @@ function generateReview(review, currUser) {
   upvoteButton.innerHTML += '&#128077;' + review.positive;
   upvoteButton.id += "up" + review.id;
   upvoteButton.addEventListener("click", () => {
+    // Users must be logged in and can only vote once. 
     if(currUser != null && !review.voters.includes(currUser)) {
       review.positive += 1;
       voteOnReview(review);
@@ -491,6 +492,7 @@ function generateReview(review, currUser) {
   downvoteButton.innerHTML += '&#128078;' + review.negative;
   downvoteButton.id += "down" + review.id;
   downvoteButton.addEventListener("click", () => {
+    // Users must be logged in and can only vote once. 
     if(currUser != null && !review.voters.includes(currUser)) {
       review.negative += 1;
       voteOnReview(review);
