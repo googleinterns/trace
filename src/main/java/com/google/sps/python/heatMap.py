4f11c6b251e7b25c2954ae054995635f94bfd8ab
Erick <enriquezerick182@gmail.com>
2020-08-14 18:13:20 +0000

    Resolve variable names in heatmap.py.

--- src/main/java/com/google/sps/python/heatMap.py
+++ src/main/java/com/google/sps/python/heatMap.py
@@ -1,9 +1,12 @@
 import numpy as np
 import json
-popFile = 'datasets/county-population.txt'
-caseFile = 'datasets/by-county-and-date.txt'
-coordFile = 'datasets/usa_county_wise.txt'
-ignoreLocations = [96, 193, 1863]  # Edge case locations where I know the population is 0 (ex: Princess Cruise Ship)
+_POPULATION = 'datasets/county-population.txt'
+_CASES = 'datasets/by-county-and-date.txt'
+_COORDINATES = 'datasets/usa_county_wise.txt'
+_WADE_HAMPTON_CENSUS_AK = 96
+_GRAND_PRINCESS_CRUISE_CA = 193
+_NEW_YORK_CITY_UNALLOCATED = 1864
+_IGNORE_LOCATIONS = [_WADE_HAMPTON_CENSUS_AK, _GRAND_PRINCESS_CRUISE_CA, _NEW_YORK_CITY_UNALLOCATED]
 us_state_abbrev = {
     'Alabama': 'AL',
     'Alaska': 'AK',
@@ -68,41 +71,43 @@ us_state_abbrev = {
 # python dict that maps a county's name to its coordinates
 # and a covid risk weight based on recency of outbreak
 # at that locationa and that county's population
-def loadCovidData():
-    countyToCoords = readCountyWise(coordFile)
-    popMap = loadPopMap(popFile, countyToCoords)
-    weightedCases = loadWeightedCases(caseFile, popMap)
+def load_covid_data():
+    county_to_coordinates = read_county_wise()
+    population_map = load_population_map(county_to_coordinates)
+    weighted_cases = load_weighted_cases(population_map)
     i = 0
-    for county in popMap:
-        popMap[county]["weight"] = int(weightedCases[i]
-          / popMap[county]["weight"]) # Normalizes weights by population density.
+    for county in population_map:
+        population_map[county]["weight"] = int(weighted_cases[i]
+          / population_map[county]["weight"]) # Normalizes weights by population density.
         i += 1
-    return popMap
+    print(len(population_map))
+    print(len(weighted_cases))
+    return population_map
 
 
 # Reads file to create a python dict that
 # maps county names to their coordinates.
-def readCountyWise(file):
-    countyCoords = {}
+def read_county_wise():
+    county_coordinates = {}
     line_count = 0
-    f = open(file, "r")
+    f = open(_COORDINATES, "r")
     for line in f:
         if line_count > 1:  # Skip first two lines.
             coords = {}
             row = line.split(',')
-            key = formatCountyState(row[5], row[6])
+            key = format_county_state(row[5], row[6])
             # If we find a new key.
-            if len(key) > 0 and (key not in countyCoords):
+            if len(key) > 0 and (key not in county_coordinates):
                 coords["lat"] = float(row[8])
                 coords["lng"] = float(row[9])
-                countyCoords[key] = coords
+                county_coordinates[key] = coords
         line_count += 1
-    return countyCoords
+    return county_coordinates
 
 
 # Formats a county and state such as Sonoma and Washington
 # as Sonoma, WA
-def formatCountyState(county, state):
+def format_county_state(county, state):
     key = ""
     if state in us_state_abbrev:
         key = county + ", " + us_state_abbrev[state]
@@ -111,54 +116,54 @@ def formatCountyState(county, state):
 
 # Reads county-population.txt and returns python dict that matches
 # a county's name to its coordinates and relative population (population / 100)
-def loadPopMap(file, countyToCoords):
-    popMap = {}
+def load_population_map(county_to_coordinates):
+    population_map = {}
     line_count = 0
-    f = open(file, "r")
+    f = open(_POPULATION, "r")
     for line in f:
-        if line_count > 1 and (line_count not in ignoreLocations):  # Skip first two lines.
+        if line_count > 1 and (line_count not in _IGNORE_LOCATIONS):  # Skip first two lines.
             row = line.split(',')
             # Ignores unallocated cases and locations where population = 0.
             if row[1] != "Statewide Unallocated" and int(row[-1]) != 0:
                 county = ''.join(row[1].split()[:-1])  # Removes 'county' and 'borough' from county names
                 key = county + ", " + row[2]
                 # If we know its coordinates and haven't seen it before
-                if key in countyToCoords and (key not in popMap):
-                    locationObject = {
-                        "location": countyToCoords[key],  # County coordinates
+                if key in county_to_coordinates and (key not in population_map):
+                    location_object = {
+                        "location": county_to_coordinates[key],  # County coordinates
                         "weight": int(row[-1]) / 100  # County population / 100
                         }
-                    popMap[key] = locationObject
+                    population_map[key] = location_object
                 else:
-                    ignoreLocations.append(line_count)  #Locations to be ignored when creating the weight matrix.
+                    _IGNORE_LOCATIONS.append(line_count)  #Locations to be ignored when creating the weight matrix.
         line_count += 1
-    return popMap
+    return population_map
 
 
 # This function accepts a file that lists the total cases of a county on each consecutive date
 # and returns a numpy array where each entry represents the weight of a given U.S.
 # county where weight = sum(newCasePerDay * index) so as to weight more recent cases heavier than
 # older ones.
-def loadWeightedCases(file, popMap):
-    caseMatrix = []  # Each row is a county, each column is the total for given day.
+def load_weighted_cases(population_map):
+    case_matrix = []  # Each row is a county, each column is the total for given day.
     line_count = 0
-    f = open(file, "r")
+    f = open(_CASES, "r")
     for line in f:
         if line_count != 0:  # skip first line
             row = line.split(',')
             county = ''.join(row[1].split()[:-1])  # Remove 'county' and 'borough' from county name
             key = county + ", " + row[2]
-            if key in popMap and (line_count not in ignoreLocations):
-                caseMatrix.append([int(num) for num in row[3:]])  # List of daily total for this county
+            if key in population_map and (line_count not in _IGNORE_LOCATIONS):
+                case_matrix.append([int(num) for num in row[3:]])  # List of daily total for this county
         line_count += 1
-    newCaseMatrix = np.diff(np.array(caseMatrix))  #  New cases per day per county
+    new_cases_matrix = np.diff(np.array(case_matrix))  #  New cases per day per county
 
     # Multiplies each new case total by its index in its subarray to weight more recent cases heavier
-    weightedCases = np.array([np.arange(len(sublist)).dot(sublist) for sublist in newCaseMatrix])  # len = num_counties
-    return weightedCases
+    weighted_cases = np.array([np.arange(len(sublist)).dot(sublist) for sublist in new_cases_matrix])  # len = num_counties
+    return weighted_cases
 
 
-coordWeightMap = loadCovidData()
-dumpFilePath = 'heatWeights.txt'
-with open(dumpFilePath, 'w') as outfile:
-    json.dump(coordWeightMap, outfile)
+coord_weight_map = load_covid_data()
+dump_file_path = 'heatWeights.txt'
+with open(dump_file_path, 'w') as outfile:
+    json.dump(coord_weight_map, outfile)
