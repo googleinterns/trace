8d4e9b1d7ab4738da0313294406fbae64c5a6136
Dominic <dterrones31@gmail.com>
2020-08-17 21:58:29 +0000

    Compilation errors fix.

--- src/main/java/com/google/sps/data/Comment.java
+++ src/main/java/com/google/sps/data/Comment.java
@@ -134,7 +134,7 @@ public class Comment {
    * Updates private rating variable
    */
   private void updateRating(double newRating) {
-    this.rating = newRating;
+    this.stars = newRating;
   }
 
   /** 
