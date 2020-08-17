ddd9ac11a6454b43310d66b8d681353331535a40
Dominic <dterrones31@gmail.com>
2020-08-17 21:40:13 +0000

    Updated constructor.

--- src/main/java/com/google/sps/data/Comment.java
+++ src/main/java/com/google/sps/data/Comment.java
@@ -82,6 +82,7 @@ public class Comment {
     this.author = author;
     this.messageContent = message;
     this.timestamp = timestamp;
+    this.stars = rating;
     this.positive = 0L;
     this.negative = 0L;
     this.positiveVoters = new HashSet<>();
