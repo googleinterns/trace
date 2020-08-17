67398943f444277002bbf7f1628da0a0848ba453
Dominic <dterrones31@gmail.com>
2020-08-17 21:43:41 +0000

    Add updater method for rating.

--- src/main/java/com/google/sps/data/Comment.java
+++ src/main/java/com/google/sps/data/Comment.java
@@ -126,6 +126,14 @@ public class Comment {
     return this.stars;
   }
 
+  /**
+   * Rating updater method
+   * Updates private rating variable
+   */
+  private void updateRating(double newRating) {
+    this.rating = newRating;
+  }
+
   /** 
    * Time Accessor method
    * Accesses private variable
