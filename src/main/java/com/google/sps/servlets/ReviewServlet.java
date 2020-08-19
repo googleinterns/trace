2737979b75fe93d92814211fd50e881eb85aa0b7
claudia <cjcoulibaly@gmail.com>
2020-08-19 17:33:09 +0000

    Added a comment

--- src/main/java/com/google/sps/servlets/ReviewServlet.java
+++ src/main/java/com/google/sps/servlets/ReviewServlet.java
@@ -68,6 +68,7 @@ public class ReviewServlet extends HttpServlet {
       Comment com = new Comment(author, message, timestamp, positive, negative);
       com.setId(id);
       currentPlace.addReview(com);
+      // If the user is logged in, add their voting status to the comment.
       if (currUser != null){
         addVote(id, com, currUser);
       }
