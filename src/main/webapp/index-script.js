b28db2957426c97a84b47d47893b8895582c9459
Dominic <dterrones31@gmail.com>
2020-08-24 21:56:01 +0000

    Change JS activation script to recognize elements by id, not CSS class.

--- src/main/webapp/index-script.js
+++ src/main/webapp/index-script.js
@@ -86,9 +86,9 @@ function returnToReviewScreen() {
 
 /* Adds mouse listeners to searchBar-related html items. */
 function activateSearchBar() {
-  const searchIcon = document.querySelector("#search-icon");
-  const searchBar = document.querySelector(".search");
-  const searchForm = document.querySelector("#searchForm");
+  const searchIcon = document.getElementbyID("search-icon");
+  const searchBar = document.getElementbyID("search");
+  const searchForm = document.getElementbyID("searchForm");
   const clearIcon = document.querySelector(".clear-icon");
 
   // Make 'clear-icon' visible when user starts typing.
